b7cc07dc98a1044e73a2790f1359e5d187e1d766
Todd Miller <toddses@gmail.com>
2016-09-21 16:18:47 -0400

    use es2015 template strings

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,3 +1,8 @@
-function getView (view){
-    return 'views/'+view+'/'+view+'.html';
+/**
+ * Retrieve the string URL of a view or directive template
+ * @param  {string} filename Name of the html file to retrieve
+ * @return {string}          Relative URL to the minified template
+ */
+function getView (filename){
+  return `dist/html/${filename}.min.html`;
 }
